aa935e1e5fcc474522c15a77e18ffd002074db97
jpenlan <79890843+jpenlan@users.noreply.github.com>
2023-05-14 18:47:34 -0700

    Finished

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ Jacob Penland
 Garden Defender
 Hours: ~40 Hours
 Regarding programming, I feel the use of paths and time based movement on animated sprites to create the illusion of a 3D environment was particularly unique. I certainly could have made it more elegant (particularly the update() function in Play.js), but it did take a lot of work to make, and for that I'm proud of it.
-Regrarding art, I took a good amount of time to make every asset, animations included, and created an atlas for multiple sprite sheets. Additionally the art was meant to work in tandem with the code to create the illusion of a 3D envrionment, and for the most part I think I did that well.
-/*
+Regarding art, I took a good amount of time to make every asset, animations included, and created an atlas for multiple sprite sheets. Additionally the art was meant to work in tandem with the code to create the illusion of a 3D envrionment, and for the most part I think I did that well.
+*/
 
 let config = {
     type: Phaser.AUTO,
